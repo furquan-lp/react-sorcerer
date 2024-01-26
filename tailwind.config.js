24dfbb665de10a82f138461c0d1c41ffe51c550c
Syed F Ahmad <furquan@protonmail.com>
2024-01-26 17:56:23 +0530

    Added basic Nord color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'nord0': '#2e3440',
+      'nord1': '#3b4252',
+      'nord2': '#434c5e',
+      'nord3': '#4c566a'
+    },
     extend: {},
   },
   plugins: {
