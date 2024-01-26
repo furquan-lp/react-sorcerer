647749f3e89fa30705c10a18d629d014f9f0a748
Syed F Ahmad <furquan@protonmail.com>
2024-01-27 00:34:56 +0530

    Added a basic header with a simple text logo and text

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className='flex bg-nord0 p-2.5'>
+      <span className='md:w-2/3 md:mx-auto text-3xl'>
+        <span className='font-logo font-bold select-none'>sorcerer.live</span>
+        <span className='font-logotext pl-8 text-nord4'>a magician never reveals his secrets</span>
+      </span>
+    </header>
+  );
+}
