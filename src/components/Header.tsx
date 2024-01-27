1688788926c1561a67e2fe2cf6cbacf5665320e6
Syed F Ahmad <furquan@protonmail.com>
2024-01-27 15:42:50 +0530

    Added a transparent white logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 export default function Header() {
   return (
     <header className='flex bg-nord0 p-2.5'>
-      <span className='md:w-2/3 md:mx-auto text-3xl'>
+      <span className='flex items-center md:w-2/3 md:mx-auto text-3xl'>
+        <img src='logo-transparent-medium.png' className='max-w-14 p-1 mx-2' alt='wizard hat logo' />
         <span className='font-logo font-bold select-none'>sorcerer.live</span>
         <span className='font-logotext pl-8 text-nord4'>a magician never reveals his secrets</span>
       </span>
