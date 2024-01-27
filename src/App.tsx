ba2f4b240a42a28b86e5d2c743f5f5938a768a4b
Syed F Ahmad <furquan@protonmail.com>
2024-01-27 21:27:41 +0530

    Added the RSEditor to App with a fixed width article container around it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import RSEditor from "./components/Editor";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className='bg-nord1 text-nord6 h-screen max-w-full'>
       <Header />
-      <div className='p-10'>Hello!</div>
+      <article className='md:w-1/2 md:mx-auto'>
+        <RSEditor />
+      </article>
     </div>
   );
 };
