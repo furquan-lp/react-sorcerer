77c85bae88aea48d57f98d98e753b40f284079c0
Syed F Ahmad <furquan@protonmail.com>
2024-01-30 22:20:50 +0530

    Added some padding on article for smaller screens and change h to min-h on the main container

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='bg-nord1 text-nord6 h-screen max-w-full'>
+    <div className='bg-nord1 text-nord6 min-h-screen max-w-full'>
       <Header />
-      <article className='md:w-1/2 md:mx-auto'>
+      <article className='md:w-1/2 md:mx-auto md:p-0 p-2'>
         <RSEditor />
       </article>
     </div>
