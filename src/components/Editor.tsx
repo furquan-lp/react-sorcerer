27f263f396177ac6e383ea8e41eb2a83b9f8917b
Syed F Ahmad <furquan@protonmail.com>
2024-02-05 14:27:13 +0530

    Added a custom decocrator with a regular expression strategy for bold styling

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,12 +1,35 @@
 import { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, CompositeDecorator, ContentBlock, ContentState } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
+const BoldSpan = ({ offsetKey, children }: { offsetKey: string, children: React.ReactNode }) => {
+  return (
+    <span className='font-bold' key={offsetKey}>{children}</span>
+  )
+};
+
 export default function RSEditor() {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const decorator: CompositeDecorator = new CompositeDecorator([{
+    strategy: findWithRegex, component: BoldSpan
+  }])
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty(decorator));
+
+  function findWithRegex(contentBlock: ContentBlock, callback: (start: number, end: number) => void,
+    contentState: ContentState) {
+    const BOLD_REGEX: RegExp = /\*\*[^*]+\*\*/g;
+
+    const text = contentBlock.getText();
+    let matchArr;
+    let start = 0;
+
+    while ((matchArr = BOLD_REGEX.exec(text)) !== null) {
+      start = matchArr.index;
+      callback(start, start + matchArr[0].length);
+    }
+  }
 
   return (
-    <div className='my-10 p-6 shadow shadow-nord0 rounded bg-nord4 text-nord0'>
+    <div className='my-10 p-6 shadow shadow-nord0 rounded bg-nord4 text-nord0 min-h-96 max-h-[75vh] overflow-scroll'>
       <Editor editorState={editorState} onChange={setEditorState} />
     </div>
   );
