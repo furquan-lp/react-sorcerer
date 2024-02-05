095e83b4224ef97cb4a5643dc50e9f11bb828adb
Syed F Ahmad <furquan@protonmail.com>
2024-02-05 18:33:39 +0530

    Added the 'Frost' Nord colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ export default {
         'nord3': '#4c566a',
         'nord4': '#d8dee9',
         'nord5': '#e5e9f0',
-        'nord6': '#eceff4'
+        'nord6': '#eceff4',
+        'nord7': '#8fbcbb',
+        'nord8': '#88c0d0',
+        'nord9': '#81a1c1',
+        'nord10': '#5e81ac',
       },
       fontFamily: {
         'logo': ['Handlee', 'cursive'],
