a2dcc22aa89ee919eaf8eec836bc05c1f81ed8b1
Syed F Ahmad <furquan@protonmail.com>
2024-02-05 17:07:01 +0530

    Added a getDecorators function to create an array of DraftDecorators from style strings regexes using a higher order function to make the strategy functions

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,51 @@
 import { useState } from 'react';
-import { Editor, EditorState, CompositeDecorator, ContentBlock, ContentState } from 'draft-js';
+import { Editor, EditorState, CompositeDecorator, ContentBlock, ContentState, DraftDecoratorComponentProps, DraftDecorator } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
+//const StyledSpan = ({ style, offsetKey, children }: {
+//  style: string, offsetKey: string, children: React.ReactNode
+//}) => {
+//  return (
+//    <span className={style} key={offsetKey}>{children}</span>
+//  );
+//};
+
+function getDecorators(): DraftDecorator<any>[] {
+  const richStyles: [RegExp, string][] = [
+    [/\*\*[^*]+\*\*/g, 'font-bold'],
+    [/\*[^*]+\*/g, 'italic']
+  ]
+
+  function findWithRegex(regex: RegExp): (
+    block: ContentBlock,
+    callback: (start: number, end: number) => void,
+    contentState: ContentState,
+  ) => void {
+    return (contentBlock: ContentBlock, callback: (start: number, end: number) => void, contentState: ContentState) => {
+      const text = contentBlock.getText();
+      let matchArr;
+      let start = 0;
+
+      while ((matchArr = regex.exec(text)) !== null) {
+        start = matchArr.index;
+        callback(start, start + matchArr[0].length);
+      }
+    }
+  }
+
+  return richStyles.map(e => {
+    const StyledSpan: React.FC<Pick<DraftDecoratorComponentProps, 'offsetKey' | 'children'>> = ({ offsetKey,
+      children }) => {
+      return (
+        <span className={e[1]} key={offsetKey}>{children}</span>
+      );
+    }
+    return {
+      strategy: findWithRegex(e[0]),
+      component: StyledSpan
+    }
+  });
+}
 const BoldSpan = ({ offsetKey, children }: { offsetKey: string, children: React.ReactNode }) => {
   return (
     <span className='font-bold' key={offsetKey}>{children}</span>
@@ -9,24 +53,8 @@ const BoldSpan = ({ offsetKey, children }: { offsetKey: string, children: React.
 };
 
 export default function RSEditor() {
-  const decorator: CompositeDecorator = new CompositeDecorator([{
-    strategy: findWithRegex, component: BoldSpan
-  }])
+  const decorator: CompositeDecorator = new CompositeDecorator(getDecorators())
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty(decorator));
-
-  function findWithRegex(contentBlock: ContentBlock, callback: (start: number, end: number) => void,
-    contentState: ContentState) {
-    const BOLD_REGEX: RegExp = /\*\*[^*]+\*\*/g;
-
-    const text = contentBlock.getText();
-    let matchArr;
-    let start = 0;
-
-    while ((matchArr = BOLD_REGEX.exec(text)) !== null) {
-      start = matchArr.index;
-      callback(start, start + matchArr[0].length);
-    }
-  }
 
   return (
     <div className='my-10 p-6 shadow shadow-nord0 rounded bg-nord4 text-nord0 min-h-96 max-h-[75vh] overflow-scroll'>
