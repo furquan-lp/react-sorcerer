914c05e3198cb3ba47f8ed85c08a7d58e1cf4a01
Syed F Ahmad <furquan@protonmail.com>
2024-02-08 10:25:26 +0530

    Added Nord theme colors in the Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,28 +6,36 @@ export default function Footer() {
           <span>Copyright &copy; 2024 Syed Furquan Ahmad</span>
           <span>Powered by DigitalOcean</span>
           <span>This website is licensed under the Apache License 2.0. See the <a
-            href="https://raw.githubusercontent.com/furquan-lp/react-sorcerer/master/LICENSE"
-            className="underline hover:no-underline">LICENSE</a> for more details.
+            href='https://raw.githubusercontent.com/furquan-lp/react-sorcerer/master/LICENSE'
+            className='underline hover:no-underline'>LICENSE</a> for more details.
+          </span>
+          <span className='flex gap-x-1'>
+            <span>This site uses the <a href='https://www.nordtheme.com/' className='underline hover:no-underline'>Nord
+              theme</a> colors</span>
+            <span className='p-2.5 m-1.5 md:p-2 md:m-0.5 rounded-full bg-nord7' />
+            <span className='p-2.5 m-1.5 md:p-2 md:m-0.5 rounded-full bg-nord8' />
+            <span className='p-2.5 m-1.5 md:p-2 md:m-0.5 rounded-full bg-nord9' />
+            <span className='p-2.5 m-1.5 md:p-2 md:m-0.5 rounded-full bg-nord7' />
           </span>
         </div>
         <div className='flex flex-col gap-y-4 text-sm md:text-base'>
           <span className='flex flex-col gap-y-2 md:flex-row justify-between gap-x-1 md:gap-x-10'>
-            <a href="https://frontend.nextdev.in/" className='hover:text-nord7 hover:underline transition-all
+            <a href='https://frontend.nextdev.in/' className='hover:text-nord7 hover:underline transition-all
              delay-200'>
               More Frontend
             </a>
-            <a href="https://nextdev.in/" className='hover:text-nord7 hover:underline transition-all
+            <a href='https://nextdev.in/' className='hover:text-nord7 hover:underline transition-all
              delay-200'>
               NEXTDEV
             </a>
           </span>
           <hr className='border-nord3' />
           <span className='flex flex-col gap-y-2 md:flex-row justify-between gap-x-1 md:gap-x-10'>
-            <a href="https://api.nextdev.in/resume.pdf" className='hover:text-nord7 hover:underline transition-all
+            <a href='https://api.nextdev.in/resume.pdf' className='hover:text-nord7 hover:underline transition-all
              delay-200'>
               Resume
             </a>
-            <a href="mailto:syed@nextdev.in" className='hover:text-nord7 hover:underline transition-all
+            <a href='mailto:syed@nextdev.in' className='hover:text-nord7 hover:underline transition-all
              delay-200'>
               Contact
             </a>
